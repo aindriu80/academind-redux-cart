850aec5853cefd03c7dfed588555ce7b2e19ba52
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-02 15:31:36 +0100

    Using an Action Creator Thunk

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { uiActions } from './ui-slice'
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -38,6 +39,48 @@ const cartSlice = createSlice({
     },
   },
 })
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    )
+    const sendRequest = async (request) => {
+      const response = await fetch(
+        'https://academind-react-databases-app-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      )
+
+      if (!response.ok) {
+        throw new Error('Sending cart data has failed')
+      }
+    }
+
+    try {
+      await sendRequest()
+
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success...',
+          message: 'Sent cart data successfully!',
+        })
+      )
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending cart data has failed!',
+        })
+      )
+    }
+  }
+}
 
 export const cartActions = cartSlice.actions
 
