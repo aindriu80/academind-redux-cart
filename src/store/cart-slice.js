22fd7981d71cb1fd4f1c0d0ce4125dea9db24043
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-03 10:22:58 +0100

    Retrieving data from Firebase - initial

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { uiActions } from './ui-slice'
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -9,6 +8,10 @@ const cartSlice = createSlice({
     totalAmount: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity
+      state.items = action.payload.items
+    },
     addItemToCart(state, action) {
       const newItem = action.payload
       const existingItem = state.items.find((item) => item.id === newItem.id)
@@ -39,48 +42,6 @@ const cartSlice = createSlice({
     },
   },
 })
-
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      })
-    )
-    const sendRequest = async (request) => {
-      const response = await fetch(
-        'https://academind-react-databases-app-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        { method: 'PUT', body: JSON.stringify(cart) }
-      )
-
-      if (!response.ok) {
-        throw new Error('Sending cart data has failed')
-      }
-    }
-
-    try {
-      await sendRequest()
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success...',
-          message: 'Sent cart data successfully!',
-        })
-      )
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data has failed!',
-        })
-      )
-    }
-  }
-}
 
 export const cartActions = cartSlice.actions
 
